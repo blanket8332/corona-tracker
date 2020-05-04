ad1307ffce2d54c1480dd7f4382d7ccf2df8dca8
mac3322 <blanket8332@yahoo.com>
2020-05-04 18:09:20 +0300

    edit chart styles

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -14,54 +14,56 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
   }, []);
 
   const lineChart = dailyData.length ? (
-    <Line
-      data={{
-        labels: dailyData.map(({ date }) => date),
-        datasets: [
-          {
-            data: dailyData.map(({ confirmed }) => confirmed),
-            label: "Infected",
-            borderColor: "#3333ff",
-            fill: true,
-          },
-          {
-            data: dailyData.map(({ deaths }) => deaths),
-            label: "Deaths",
-            borderColor: "red",
-            backgroundColor: "rgba(255, 0, 0, 0.4)",
-            fill: true,
-          },
-        ],
-      }}
-    />
+    <div className={styles.line_container}>
+      <Line
+        data={{
+          labels: dailyData.map(({ date }) => date),
+          datasets: [
+            {
+              data: dailyData.map(({ confirmed }) => confirmed),
+              label: "Infected",
+              borderColor: "#3333ff",
+              fill: true,
+            },
+            {
+              data: dailyData.map(({ deaths }) => deaths),
+              label: "Deaths",
+              borderColor: "red",
+              backgroundColor: "rgba(255, 0, 0, 0.4)",
+              fill: true,
+            },
+          ],
+        }}
+      />
+    </div>
   ) : null;
 
   const barChart = confirmed ? (
-    <Bar
-      data={{
-        labels: ["Infected", "Recovered", "Deaths"],
-        datasets: [
-          {
-            label: "People",
-            backgroundColor: [
-              "rgba(0, 0, 255, 0.4)",
-              "rgba(0, 255, 0, 0.4)",
-              "rgba(255, 0, 0, 0.4)",
-            ],
-            data: [confirmed.value, recovered.value, deaths.value],
-          },
-        ],
-      }}
-      options={{
-        legend: { display: false },
-        title: { display: true, text: `Current state in ${country}` },
-      }}
-    />
+    <div className={styles.bar_container}>
+      <Bar
+        data={{
+          labels: ["Infected", "Recovered", "Deaths"],
+          datasets: [
+            {
+              label: "People",
+              backgroundColor: [
+                "rgba(0, 0, 255, 0.4)",
+                "rgba(0, 255, 0, 0.4)",
+                "rgba(255, 0, 0, 0.4)",
+              ],
+              data: [confirmed.value, recovered.value, deaths.value],
+            },
+          ],
+        }}
+        options={{
+          legend: { display: false },
+          title: { display: true, text: `Current state in ${country}` },
+        }}
+      />
+    </div>
   ) : null;
 
-  return (
-    <div className={styles.container}>{country ? barChart : lineChart}</div>
-  );
+  return <>{country ? barChart : lineChart}</>;
 };
 
 export default Chart;
